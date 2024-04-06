bc2d6f4bafe0e92cf091e1b6e8785a21015232d1
Somesh Kumar <someshkumarsahoo@gmail.com>
2024-04-06 21:10:59 +0530

    Looping through player chances

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 def Board():
     zero = 'X' if xstate[0] else 'O' if ostate[0] else 0
-    one = 'X' if xstate[1] else 'O' if ostate[1] else 0
-    two = 'X' if xstate[2] else 'O' if ostate[2] else 0
-    three = 'X' if xstate[3] else 'O' if ostate[3] else 0
-    four = 'X' if xstate[4] else 'O' if ostate[4] else 0
-    five = 'X' if xstate[5] else 'O' if ostate[5] else 0
-    six = 'X' if xstate[6] else 'O' if ostate[6] else 0
-    seven = 'X' if xstate[7] else 'O' if ostate[7] else 0
-    eight = 'X' if xstate[8] else 'O' if ostate[8] else 0
+    one = 'X' if xstate[1] else 'O' if ostate[1] else 1
+    two = 'X' if xstate[2] else 'O' if ostate[2] else 2
+    three = 'X' if xstate[3] else 'O' if ostate[3] else 3
+    four = 'X' if xstate[4] else 'O' if ostate[4] else 4
+    five = 'X' if xstate[5] else 'O' if ostate[5] else 5
+    six = 'X' if xstate[6] else 'O' if ostate[6] else 6
+    seven = 'X' if xstate[7] else 'O' if ostate[7] else 7
+    eight = 'X' if xstate[8] else 'O' if ostate[8] else 8
     print(f" {zero} | {one} | {two} ")
     print(f"---|---|---")
     print(f" {three} | {four} | {five} ")
@@ -31,3 +31,5 @@ if __name__ == "__main__":
             print("O's Chance")
             value = int(input("Please Enter a value to place 'O' "))
             ostate[value] = 1
+
+        turn = 1 - turn
