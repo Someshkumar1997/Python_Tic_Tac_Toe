567c821974e977417252fe5eecbb1366ce8c0623
Somesh Kumar <someshkumarsahoo@gmail.com>
2024-04-06 20:47:53 +0530

    Chance for the player

--- main.py
+++ main.py
@@ -10,5 +10,16 @@ def Board():
 if __name__ == "__main__":
     xstate = [0, 0, 0, 0, 0, 0, 0, 0]
     ostate = [0, 0, 0, 0, 0, 0, 0, 0]
+    turn = 1 # 1 for 'x' and 0 for 'o'
     print("Welcome to TIC TAC TOE !")
-    Board()
+    while True:
+        if turn == 1:
+            print("X's Chance")
+            value = input("Please Enter a value to place 'X'")
+            xstate[value] = 1
+        else:
+            print("O's Chance")
+            value = input("Please Enter a value to place 'O'")
+            ostate[value] = 1
+        Board()
+        break
