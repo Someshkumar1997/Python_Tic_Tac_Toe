16dbc770b3031d94f46569a8919ce258b437b61c
Somesh Kumar <someshkumarsahoo@gmail.com>
2024-04-06 20:40:41 +0530

    Created Board function to print

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def Board():
+    print(f" 0 | 1 | 2 ")
+    print(f"---|---|---")
+    print(f" 3 | 4 | 5 ")
+    print(f"---|---|---")
+    print(f" 6 | 7 | 8 ")
+
+
+if __name__ == "__main":
+    xstate = [0, 0, 0, 0, 0, 0, 0, 0]
+    ostate = [0, 0, 0, 0, 0, 0, 0, 0]
+    print("Welcome to TIC TAC TOE !")
+    Board()
