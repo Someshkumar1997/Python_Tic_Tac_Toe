543f016ad4c2a7d4211a99e23adbeb325691fc4d
Somesh Kumar <someshkumarsahoo@gmail.com>
2024-04-06 20:49:29 +0530

    Fixed error for index position

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     turn = 1 # 1 for 'x' and 0 for 'o'
     print("Welcome to TIC TAC TOE !")
     while True:
+        Board()
         if turn == 1:
             print("X's Chance")
-            value = input("Please Enter a value to place 'X'")
+            value = int(input("Please Enter a value to place 'X'"))
             xstate[value] = 1
         else:
             print("O's Chance")
-            value = input("Please Enter a value to place 'O'")
+            value = int(input("Please Enter a value to place 'O'"))
             ostate[value] = 1
-        Board()
         break
